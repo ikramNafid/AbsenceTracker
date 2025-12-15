e485324746b5aa56b4aba89d70e4a58029839b36
Ikram Nafid <ikraminformaticienne2003@gmail.com>
2025-12-15 13:56:07 +0000

    Test modification Absence model

--- lib/models/session_model.dart
+++ lib/models/session_model.dart
@@ -1,13 +1,1 @@
-class SessionModel {
-  final int id;
-  final String moduleName;
-  final String date; // You can switch to DateTime if you prefer.
-  final String time;
 
-  SessionModel({
-    required this.id,
-    required this.moduleName,
-    required this.date,
-    required this.time,
-  });
-}
